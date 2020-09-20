87d688349a968dd3b42d9d7b0a36fdb43539507d
Renan Sies <renansies@gmail.com>
2020-09-20 17:38:36 -0300

    add unique constraint for username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,10 +10,18 @@ export class UserRepository extends Repository<User> {
 
         const {username, password} = authCredentialsDto;
 
+        
         const user = new User();
         user.username = username;
         user.password = password;
 
-        await user.save();
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('username already exists');
+            }
+            throw new InternalServerErrorException();
+        }
     }
 }
